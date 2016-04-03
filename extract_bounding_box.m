cfa5ed850fbe68f75fc62f3af5fc22d73576cf8d
jianfeng Ren <jxr041100@gmail.com>
2016-04-02 21:31:08 -0700

    fixed one bug

--- extract_bounding_box.m
+++ extract_bounding_box.m
@@ -1,11 +1,12 @@
 
-function extract_bounding_box(foldername)
-extractvbbfile(foldername);
+
+
+function extract_bounding_box()
+extractvbbfile('./data');
 return    
-    
 
 function extractvbbfile(foldername)
-name=sprintf('%s\\seq\\*.vbb',foldername);
+name=sprintf('%s/seq/*.vbb',foldername);
 list1=dir(name);
 s=1;
 e=length(list1);
@@ -14,31 +15,27 @@ for i=s:e
     %seperate the ext and name.
     [pathstr,name,ext] = fileparts(filename);
     fprintf(1,'extract the ts from %s\n',filename);
-    video_name=sprintf('%s\\seq\\%s',foldername,name);
+    video_name=sprintf('%s/seq/%s',foldername,name);
     vbb_name=sprintf('%s.vbb',video_name);
     seqname=sprintf('%s.seq',video_name);
-    save_txt = sprintf('%s.txt',video_name);
     A=vbb('vbbLoad',vbb_name);
     nFrame=A.nFrame;
     sr=seqIo(seqname,'reader');
-    out = sr.next(); 
+      sr.next();
     [stats,stats1,logDur] = vbb( 'getStats', A );
-    fid = fopen(save_txt,'w');
     fprintf(1,'extracting bounding box from %s\n',filename);
     for j=1:nFrame
         [gt,posv,lbls]=vbb('frameAnn',A,j,stats.labels);       
         newName = sprintf([name,'%05d.jpg'],j);
         [I,ts]=sr.getframe();
-        if(~isempty(gt))
+        if(~isempty(gt)) 
            len=size(gt,1);
-           for m = 1:len             
-            fprintf(fid,'%s %d %d %d %d %s\n',newName,round(gt(m,1)),round(gt(m,2)),round(gt(m,1))+ round(gt(m,3)),round(gt(m,2))+ round(gt(m,4)),char(lbls{m}));
-            % write the ROI
+           for m = 1:len   
             outfolder = sprintf('%s\\Result\\%s',foldername,name);
-             if (~exist(outfolder,'dir'))    
+            if (~exist(outfolder,'dir'))    
                 mkdir(outfolder);
             end
-            outname=sprintf('%s\\%s_%05d_%02d.jpg',outfolder,char(lbls{m}),j,m);
+            outname=sprintf('%s/%s_%05d_%02d.jpg',outfolder,char(lbls{m}),j,m);
            
             x0=round(gt(m,2));
             x1=x0+round(gt(m,4));
@@ -52,16 +49,15 @@ for i=s:e
                 y1=y;   
             end
             %                image=I(gt(k,2):(gt(k,2)+gt(k,4)),gt(k,1):(gt(k,1)+gt(k,3)),:);
-            image=I(x0:x1,y0:y1,:);
+            image=I(x0:x1,y0:y1,:);           
             imwrite(image, outname, 'Quality', 100); 
-           end
-        else
-            fprintf(fid,'%s\n',newName);
-        end                        
-    end
-    fclose(fid);
-     fprintf(1,'done extracting bounding box from %s\n',filename);
+           end       
+        end
+        sr.next();
+    end   
+    fprintf(1,'done extracting bounding box from %s\n',filename);
 end
 return
 
 
+    
