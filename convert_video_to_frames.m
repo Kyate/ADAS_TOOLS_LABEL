317e3d8acc80da003a06b79f44c7462cddfe8f44
jianfeng Ren <jxr041100@gmail.com>
2016-04-07 14:24:22 -0700

    now converting the videos into frames

--- convert_video_to_frames.m
+++ convert_video_to_frames.m
@@ -2,10 +2,10 @@
 
 
 
-input_video_dir = '';
+input_video_dir = './data';
 
 %list all mp4 videos
-name=sprintf('%s\\*.mp4',input_video_dir);
+name=sprintf('%s/videos/*.avi',input_video_dir);
 list1=dir(name);
 
 s=1;
@@ -16,23 +16,27 @@ for i=s:e
     fprintf(1,'extract the frames from %s\n',filename);
     
     %create the directory to save the images. 
-    image_folder_name=sprintf('%s\\images\\%s',input_video_dir,name);
+    image_folder_name=sprintf('%s/Image/%s',input_video_dir,name);
     if (~exist(image_folder_name,'dir'))    
         mkdir(image_folder_name);
     end
     
+    video_name=sprintf('%s/videos/%s',input_video_dir,filename);
     %read the frames from video
-    vid=mmreader([input_video_dir,'\',filename]);
+    vid = VideoReader(video_name);
     numFrames = vid.NumberOfFrames;
     height = vid.Height;
     width = vid.Width;
+    fps =  vid.FrameRate;
+    quality = 100;  
     n=numFrames;
  
     frame_number = 1; 
     %alway save the first frame for each video
     prev_frame = read(vid,1);
-    vStr = num2str(frame_number, '%05d'); 
-    imwrite(prev_frame,[image_folder_name '\' name '_' vStr '.jpg']);
+    vStr = num2str(frame_number, '%05d');
+    save_image_name = [image_folder_name '/' name '_' vStr '.jpg'];
+    imwrite(prev_frame,save_image_name);
     frame_number=frame_number+1;
     for i = 2:1:n
          frame = read(vid,i);
@@ -40,8 +44,9 @@ for i=s:e
          diff_sum = sum(sum(diff));
          threshold = 20*width*height;
          if(diff_sum>threshold) || mod(i,10)==0      
-             vStr = num2str(frame_number, '%05d'); 
-             imwrite(frame,[image_folder_name '\' name '_' vStr '.jpg']);
+             vStr = num2str(frame_number, '%05d');
+             save_image_name = [image_folder_name '/' name '_' vStr '.jpg'];
+             imwrite(frame,save_image_name);
              frame_number=frame_number+1; 
              prev_frame = frame;
          end        
